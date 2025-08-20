284f99bc2d0680045be59d6d620376880af4b9bb
Daniella <apejidaniella@gmail.com>
2025-08-20 14:25:15 +0100

    add all files

---
+++ dom-manipulation/script.js
@@ -0,0 +1,34 @@
+const quotes = [
+    {
+        "text": "He who laugh last laugh best.",
+        "category": "category 1"
+    },
+    {
+        "text": "Half bread is better than none",
+        "category": "category 2"
+    }
+]
+
+function showRandomQuote() {
+    const quotesDisplay = document.getElementById("quoteDisplay")
+    
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    
+    quotesDisplay.textContent = `${randomQuote.text} ${randomQuote.category}`
+
+}
+
+function createAddQuoteForm() {
+    const newQuoteText = document.getElementById("newQuoteText")
+    const newQuoteCategory = document.getElementById("newQuoteCategory")
+    const text = newQuoteText.value.trim() || ""
+    const category = newQuoteCategory.value.trim() || ""
+    quotes.push({ text, category }) 
+    newQuoteText.value = ""
+    newQuoteCategory.value = ""
+} 
+function addQuote() {
+    createAddQuoteForm()
+}
+const newQuote = document.getElementById("newQuote")
+newQuote.addEventListener("click", showRandomQuote)
